b6666c75c90431848f366e38387b4649bd01a09c
Brittney Jackson <brnjackson@yahoo.com>
2021-07-11 14:48:50 -0400

    wrote function for rbScoring and updated checkPosition function with else if statement

--- scoring.js
+++ scoring.js
@@ -9,6 +9,36 @@ function qbScoring(qbStats) {
   return passingYardScore + passingTouchdownScore + 
   interceptionScore + rushingYardScore + rushingTouchdownScore + rushingFumbleScore
 }
+function rbScoring(rbStats) {
+    let receptionScore = rbStats.receiving.receptions
+    let rushingYardScore = rbStats.rushing.yards / 10
+    let rushingTouchdownScore = rbStats.rushing.touchdowns * 6
+    let rushingFumbleScore = rbStats.rushing.fumbles * -3
+    let recYardScore = rbStats.receiving.yards / 10
+    let recTouchdownScore = rbStats.receiving.touchdowns * 6
+    let recFumbleScore = rbStats.receiving.fumbles * -3
+    let kickReturnYardsScore = rbStats.return.kickreturn.yards / 15
+    let kickReturnTouchdownScore = rbStats.return.kickreturn.touchdowns * 6
+    let kickreturnFumbleScore = rbStats.return.kickreturn.fumbles * -3
+    let puntReturnYardsScore = rbStats.return.puntreturn.yards / 15
+    let puntreturnTouchdownScore = rbStats.return.puntreturn.touchdowns * 6
+    let puntreturnFumbleScore = rbStats.return.puntreturn.fumbles * -3
+    
+    return receptionScore +
+          rushingTouchdownScore +
+          rushingYardScore +
+          rushingFumbleScore +
+          recYardScore +
+          recTouchdownScore +
+          recFumbleScore +
+          kickReturnYardsScore +
+          kickReturnTouchdownScore +
+          kickreturnFumbleScore +
+          puntReturnYardsScore +
+          puntreturnTouchdownScore +
+          puntreturnFumbleScore
+  
+  }
 
 
 
@@ -17,6 +47,9 @@ function checkPosition(player) {
     return qbScoring(player.stats)
   }
 
+  else if (player.position === 'RB') {
+    return rbScoring(player.stats)
+  }
   else return 0
 }
 function calculateScore(player) {
