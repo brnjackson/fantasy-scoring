e086379876e88446e80e8be9cdd65e84884867a3
Brittney Jackson <brnjackson@yahoo.com>
2021-07-11 15:58:00 -0400

    confirmed all tests passed. wrote functions for all position scores

--- scoring.js
+++ scoring.js
@@ -6,25 +6,25 @@ function qbScoring(qbStats) {
   let rushingTouchdownScore = qbStats.rushing.touchdowns * 6
   let rushingFumbleScore = qbStats.rushing.fumbles * -3
 
-  return passingYardScore + passingTouchdownScore + 
+  return passingYardScore + passingTouchdownScore +
   interceptionScore + rushingYardScore + rushingTouchdownScore + rushingFumbleScore
 }
 function rbScoring(rbStats) {
-    let receptionScore = rbStats.receiving.receptions
-    let rushingYardScore = rbStats.rushing.yards / 10
-    let rushingTouchdownScore = rbStats.rushing.touchdowns * 6
-    let rushingFumbleScore = rbStats.rushing.fumbles * -3
-    let recYardScore = rbStats.receiving.yards / 10
-    let recTouchdownScore = rbStats.receiving.touchdowns * 6
-    let recFumbleScore = rbStats.receiving.fumbles * -3
-    let kickReturnYardsScore = rbStats.return.kickreturn.yards / 15
-    let kickReturnTouchdownScore = rbStats.return.kickreturn.touchdowns * 6
-    let kickreturnFumbleScore = rbStats.return.kickreturn.fumbles * -3
-    let puntReturnYardsScore = rbStats.return.puntreturn.yards / 15
-    let puntreturnTouchdownScore = rbStats.return.puntreturn.touchdowns * 6
-    let puntreturnFumbleScore = rbStats.return.puntreturn.fumbles * -3
-    
-    return receptionScore +
+  let receptionScore = rbStats.receiving.receptions
+  let rushingYardScore = rbStats.rushing.yards / 10
+  let rushingTouchdownScore = rbStats.rushing.touchdowns * 6
+  let rushingFumbleScore = rbStats.rushing.fumbles * -3
+  let recYardScore = rbStats.receiving.yards / 10
+  let recTouchdownScore = rbStats.receiving.touchdowns * 6
+  let recFumbleScore = rbStats.receiving.fumbles * -3
+  let kickReturnYardsScore = rbStats.return.kickreturn.yards / 15
+  let kickReturnTouchdownScore = rbStats.return.kickreturn.touchdowns * 6
+  let kickreturnFumbleScore = rbStats.return.kickreturn.fumbles * -3
+  let puntReturnYardsScore = rbStats.return.puntreturn.yards / 15
+  let puntreturnTouchdownScore = rbStats.return.puntreturn.touchdowns * 6
+  let puntreturnFumbleScore = rbStats.return.puntreturn.fumbles * -3
+
+  return receptionScore +
           rushingTouchdownScore +
           rushingYardScore +
           rushingFumbleScore +
@@ -37,42 +37,39 @@ function rbScoring(rbStats) {
           puntReturnYardsScore +
           puntreturnTouchdownScore +
           puntreturnFumbleScore
-  
-  }
+}
 
-  function wrScoring(wrStats) {
-    let rushingYardScore = wrStats.rushing.yards / 10
-    let rushingTouchdownScore = wrStats.rushing.touchdowns * 6
-    let rushingFumbleScore = wrStats.rushing.fumbles * -3
-    let receptionScore = wrStats.receiving.receptions
-    let receivingYardScore = wrStats.receiving.yards / 10
-    let receivingTouchdownScore = wrStats.receiving.touchdowns * 6
-    let receivingFumbleScore = wrStats.receiving.fumbles * -3
-    let kickReturnYardsScore = wrStats.return.kickreturn.yards / 15
-    let kickTouchdownScore = wrStats.return.kickreturn.touchdowns * 6
-    let kickFumbleScore = wrStats.return.kickreturn.fumbles * -3
-    let puntYardScore = wrStats.return.puntreturn.yards / 15
-    let puntTouchdownScore = wrStats.return.puntreturn.touchdowns * 6
-    let puntFumbleScore = wrStats.return.puntreturn.fumbles * -3
-  
-  
-    return rushingYardScore + rushingTouchdownScore + rushingFumbleScore
-    + receptionScore + receivingYardScore + receivingTouchdownScore
-    + receivingFumbleScore + kickReturnYardsScore + kickTouchdownScore +
+function wrScoring(wrStats) {
+  let rushingYardScore = wrStats.rushing.yards / 10
+  let rushingTouchdownScore = wrStats.rushing.touchdowns * 6
+  let rushingFumbleScore = wrStats.rushing.fumbles * -3
+  let receptionScore = wrStats.receiving.receptions
+  let receivingYardScore = wrStats.receiving.yards / 10
+  let receivingTouchdownScore = wrStats.receiving.touchdowns * 6
+  let receivingFumbleScore = wrStats.receiving.fumbles * -3
+  let kickReturnYardsScore = wrStats.return.kickreturn.yards / 15
+  let kickTouchdownScore = wrStats.return.kickreturn.touchdowns * 6
+  let kickFumbleScore = wrStats.return.kickreturn.fumbles * -3
+  let puntYardScore = wrStats.return.puntreturn.yards / 15
+  let puntTouchdownScore = wrStats.return.puntreturn.touchdowns * 6
+  let puntFumbleScore = wrStats.return.puntreturn.fumbles * -3
+
+  return rushingYardScore + rushingTouchdownScore + rushingFumbleScore +
+    receptionScore + receivingYardScore + receivingTouchdownScore +
+    receivingFumbleScore + kickReturnYardsScore + kickTouchdownScore +
     kickFumbleScore + puntYardScore + puntTouchdownScore + puntFumbleScore
-
 }
 
-function teScoring (teStats) {
-    let receptionScore = teStats.receiving.receptions
-    let receivingYardsScore = teStats.receiving.yards / 10
-    let receivingTouchdownScore = teStats.receiving.touchdowns * 6
-    let receivingFumbleScore = teStats.receiving.fumbles * -3
+function teScoring(teStats) {
+  let receptionScore = teStats.receiving.receptions
+  let receivingYardsScore = teStats.receiving.yards / 10
+  let receivingTouchdownScore = teStats.receiving.touchdowns * 6
+  let receivingFumbleScore = teStats.receiving.fumbles * -3
 
-    return receptionScore + receivingYardsScore + receivingTouchdownScore + receivingFumbleScore
+  return receptionScore + receivingYardsScore + receivingTouchdownScore + receivingFumbleScore
 }
 
-function checkPosition(player) { 
+function checkPosition(player) {
   if (player.position === 'QB') {
     return qbScoring(player.stats)
   }
@@ -80,7 +77,6 @@ function checkPosition(player) {
   else if (player.position === 'RB') {
     return rbScoring(player.stats)
   }
-
   else if (player.position === 'WR') {
     return wrScoring(player.stats)
   }
